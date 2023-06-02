1f97d42446ffedf0cf7d5c406bae2f703e2aeb67
JoseMolina <josemolimora@gmail.com>
2023-06-02 19:37:27 +0200

    cambios

---
+++ db/getDB.js
@@ -0,0 +1,31 @@
+// Ya hemos instalado el dotenv y el mysql2 desde la terminal
+// no hace falta que lo volvais instalar
+
+const mysql = require("mysql2/promise");
+
+const { MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB } = process.env;
+
+let pool;
+
+const getDB = async () => {
+  try {
+    if (!pool) {
+      pool = mysql.createPool({
+        connectionLimit: 10,
+        host: MYSQL_HOST,
+        user: MYSQL_USER,
+        password: MYSQL_PASS,
+        database: MYSQL_DB,
+        timezone: "Z",
+      });
+    }
+
+    return await pool.getConnection();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+module.exports = getDB;
+
+
---
+++ db/initDB.js
@@ -0,0 +1,11 @@
+require ('dotenv').config();
+
+const getDB = require('./getDB');
+
+const main = async () => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
