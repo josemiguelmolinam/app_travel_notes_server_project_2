80ab21f117385492fa647196652e3b6c3992eb3b
jeansktt <jean01cruz@gmail.com>
2023-06-09 13:42:49 +0200

    editando notas index y server

---
+++ controllers/notes/editNote.js
@@ -0,0 +1,39 @@
+const selectAllNotesIdQuery = require('../../db/queries/notes/selectAllNotesIdQuery');
+const { generateError } = require('../../helpers');
+const editNote = async (req, res, next) => {
+  try {
+    let { title, text, categoryId } = req.body;
+
+    if (!title && !text && !categoryId) {
+      generateError('Faltan campos', 400);
+    }
+
+    // Obtenemos la nota actual por su ID
+    const note = await selectAllNotesIdQuery(req.params.noteId);
+
+    if (!note) {
+      generateError('La nota no existe', 404);
+    }
+
+    // Establecemos los valores por defecto si no se proporcionan
+    title = title || note.title;
+    text = text || note.text;
+    categoryId = categoryId || note.categoryId;
+
+    // Actualizar los datos de la nota
+    await editNoteQuery(title, text, categoryId, req.params.noteId);
+    res.send({
+      status: 'Success',
+      message: 'Nota actualizada con éxito',
+      data: {
+        title,
+        text,
+        categoryId,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = editNote;
---
+++ db/queries/notes/editNoteQuery.js
@@ -0,0 +1,40 @@
+const getDB = require('../../getDB');
+const { generateError } = require('../../../helpers');
+
+const editNoteQuery = async (title, text, categoryId, noteId) => {
+  let connection;
+
+  try {
+    connection = await getDB();
+
+    // Verificar si ya existe una nota con el mismo título
+    let [notes] = await connection.query(
+      'SELECT id FROM notes WHERE title = ? AND id <> ?',
+      [title, noteId]
+    );
+
+    if (notes.length > 0) {
+      generateError('Ya existe una nota con este título', 403);
+    }
+
+    // Verificar si ya existe una nota con el mismo texto
+    [notes] = await connection.query(
+      'SELECT id FROM notes WHERE text = ? AND id <> ?',
+      [text, noteId]
+    );
+
+    if (notes.length > 0) {
+      generateError('Ya existe una nota con este texto', 403);
+    }
+
+    // Actualizar los datos de la nota
+    await connection.query(
+      'UPDATE notes SET title = ?, text = ?, categoryId = ?, modifiedAt = ? WHERE id = ?',
+      [title, text, categoryId, new Date(), noteId]
+    );
+  } finally {
+    if (connection) connection.release();
+  }
+};
+
+module.exports = editNoteQuery;
