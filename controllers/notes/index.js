e7816788e9f0a841c1225d984f8f1f677b324162
marcsalou23 <127138098+marcsalou23@users.noreply.github.com>
2023-06-09 22:41:21 +0200

    Me olvidé ponerlo en el índex

--- controllers/notes/index.js
+++ controllers/notes/index.js
@@ -1,6 +1,8 @@
 const newNote = require('./newNote');
 const listNotes = require('./listNotes');
 const getNotes = require('./getNotes');
+const editNote = require('./editNote');
+
 module.exports = {
   newNote,
   listNotes,
